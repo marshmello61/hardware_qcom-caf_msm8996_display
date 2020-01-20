663b69e186e67f9f3dc877c0a342dc52c74b0b0b
ashwatthama <sai404142@gmail.com>
2020-01-20 12:07:50 +0000

    remove pathmaps
    Signed-off-by: ashwatthama <sai404142@gmail.com>
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+#ifeq ($(call my-dir),$(call project-path-for,qcom-display))
 
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
@@ -22,4 +22,4 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
 endif
 endif
 
-endif
+#endif
